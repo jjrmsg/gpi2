a43e6443b9963ea1aafea43846f0cd914758970d
colomagsi2 <neomichael87@gmail.com>
2015-03-10 13:28:12 +0100

    Pruebas unitarias práctica

---
+++ ptesting/Michael Coloma/Bank/Bank/BankAccount.cs
@@ -0,0 +1,83 @@
+﻿/*///////////////////////////////////////////////////////////////////////////////////////////
+ * 
+Nota: No he creado la clase runner para realizar los test a las dos clases, ya que 
+ * que nos servimos del mismo visual studio para poder realizar las pruebas a través de
+ * su propias herramientas, en este caso tiene una pestaña "PRUEBA" donde seleccionaremos
+ * ventanas--> ejecutar todas
+ *Todas las pruebas superadas tendran un tick verde y las malas tendrán una x roja.
+ * 
+ * //////////////////////////////////////////////////////////////////////////////////////////
+*/
+using System;
+
+namespace BankAccountNS
+{
+    public class BankAccount
+    {
+        private string m_customerName;
+
+        private double m_balance;
+
+        private bool m_frozen = false;
+
+        private BankAccount()
+        {
+        }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance -= amount;
+        }
+
+        public void Credit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance += amount;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+
+    }
+}
---
+++ ptesting/Michael Coloma/Bank/BankTests/BankAccountTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        //método comprobando valores fuera de rango
+        //unit test method
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+            account.Debit(debitAmount);
+
+            // assert is handled by ExpectedException
+        }
+
+
+
+    }
+}
